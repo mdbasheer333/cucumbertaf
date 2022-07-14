48fe86dad19de4c5fe2afe95b4029242c2f5f9ac
mdbasheer333 <mdbasheer333@gmail.com>
2022-07-14 11:25:24 +0530

    parallel feature

--- src/test/java/org/cucumbertaf/runner/RunParallelCucumberTest.java
+++ src/test/java/org/cucumbertaf/runner/RunParallelCucumberTest.java
@@ -1,6 +1,5 @@
 package org.cucumbertaf.runner;
 
-import io.cucumber.java.*;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
@@ -8,7 +7,13 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json"},
         features = {"src/test/resources"},
-        glue = {"org.cucumbertaf.stepdefs", "org.cucumbertaf.hooks"})
-public class RunCucumberTest extends AbstractTestNGCucumberTests {
+        glue = {"org.cucumbertaf.stepdefs","org.cucumbertaf.hooks"})
+public class RunParallelCucumberTest extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 }
