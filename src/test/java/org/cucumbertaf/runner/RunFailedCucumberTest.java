5cfce6d6dc51500ef13e710126b9cadf9c9377c8
mdbasheer333 <mdbasheer333@gmail.com>
2022-07-16 22:43:31 +0530

    fail re run feature

--- src/test/java/org/cucumbertaf/runner/RunFailedCucumberTest.java
+++ src/test/java/org/cucumbertaf/runner/RunFailedCucumberTest.java
@@ -1,14 +1,12 @@
 package org.cucumbertaf.runner;
 
-import io.cucumber.java.*;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json", "rerun:target/failedrerun.txt"},
-        features = {"src/test/resources"},
+        features = {"@target/failedrerun.txt"},
         glue = {"org.cucumbertaf.stepdefs", "org.cucumbertaf.hooks"})
-public class RunCucumberTest extends AbstractTestNGCucumberTests {
+public class RunFailedCucumberTest extends AbstractTestNGCucumberTests {
 
 }
--- src/test/java/org/cucumbertaf/runner/RunParallelCucumberTest.java
+++ src/test/java/org/cucumbertaf/runner/RunParallelCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json"},
+        plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json", "rerun:target/failedrerun.txt"},
         features = {"src/test/resources"},
         glue = {"org.cucumbertaf.stepdefs","org.cucumbertaf.hooks"})
 public class RunParallelCucumberTest extends AbstractTestNGCucumberTests {
