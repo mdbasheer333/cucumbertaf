24758ecba00154404885e5415b1b8b583e8e54e6
mdbasheer333 <mdbasheer333@gmail.com>
2022-07-16 16:12:25 +0530

    added webtable template class

---
+++ src/main/java/org/cucumbertaf/utils/webutils/WebTable.java
@@ -0,0 +1,13 @@
+package org.cucumbertaf.utils.webutils;
+
+import org.openqa.selenium.WebElement;
+
+public class WebTable {
+
+    WebElement element;
+
+    public WebTable(WebElement element){
+        this.element=element;
+    }
+
+}
