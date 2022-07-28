557adf22c6c0a42dfae241c3cb1fb25b41b63ab4
mdbasheer333 <mdbasheer333@gmail.com>
2022-07-28 22:43:01 +0530

    demo give

--- src/main/java/org/cucumbertaf/corelib/DriverClass.java
+++ src/main/java/org/cucumbertaf/corelib/DriverClass.java
@@ -8,45 +8,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class DriverClass {
     private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         return driverThreadLocal.get();
     }
 
-    public static void setDriverNull(){
-        if(getDriver()!=null)
+    public static void setDriverNull() {
+        if (getDriver() != null)
             getDriver().quit();
         driverThreadLocal.set(null);
     }
 
     public static WebDriver getDriverInstance(String browserName) throws Exception {
-
-        if(driverThreadLocal.get()!=null){
+        if (driverThreadLocal.get() != null) {
             return getDriver();
         }
-
         switch (browserName) {
             case "chrome":
             case "gc":
                 System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
                 driverThreadLocal.set(new ChromeDriver());
-                //driverThreadLocal.get().manage().window().maximize();
                 break;
             case "edge":
             case "msedge":
                 System.setProperty("webdriver.edge.driver", "src/test/resources/drivers/msedgedriver.exe");
                 driverThreadLocal.set(new EdgeDriver());
-//                driver = new EdgeDriver();
-//                driver.manage().window().maximize();
                 break;
             case "ff":
             case "firefox":
                 System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
                 driverThreadLocal.set(new FirefoxDriver());
-//                driver = new FirefoxDriver();
-//                driver.manage().window().maximize();
                 break;
             default:
-                throw new Exception("Invalid browser provided "  + browserName);
+                throw new RuntimeException("Invalid browser name provided " + browserName);
         }
         driverThreadLocal.get().manage().window().maximize();
         return driverThreadLocal.get();
--- src/test/resources/features/registration/register.feature
+++ src/test/resources/features/registration/register.feature
@@ -1,13 +1,13 @@
 Feature: this feature is to register a user
 
   @Registration
-  Scenario Outline: the user registration
+  Scenario: the user registration
     Given user launches application
     When register page should displayed
     And fill all the details in registration page
     When submits the details
     Then registration should be success
-    Examples:
-      | Iteration |
-      | 1         |
-      | 2         |
+#    Examples:
+#      | Iteration |
+#      | 1         |
+#      | 2         |
